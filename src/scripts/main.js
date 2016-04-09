49b4bc29a15b89471d4fedd5525c0f6c9444719d
alexander <aleksandr.dankov@popel.com.ua>
2016-04-09 21:36:33 +0300

    first commit

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -16,4 +16,34 @@
             };
         }
     });
+    $('.works-slider').bxSlider({
+        minSlides: 3,
+        maxSlides:3,
+        slideWidth:115,
+        slideMargin: 20,
+        pager:false,
+        hideControlOnEnd:true,
+        infiniteLoop: false
+    });
+    $('.guest-book-slider').bxSlider({
+        minSlides: 3,
+        maxSlides:3,
+        slideWidth:204,
+        slideMargin: 31,
+        pager:false,
+        hideControlOnEnd:true,
+        infiniteLoop: false
+    });
+
+    $(document).on('click', '.artist-name', function(){
+        $(this).parent().find('.active').removeClass('active');
+        $(this).addClass('active');
+        $('.artist-wave').animate({
+            left: $(this).position().left + $(this).width() * 0.55
+        })
+    });
+    $('.btn-primary').on('click', function(e){
+        e.preventDefault();
+    });
+
 })(window.jQuery);
