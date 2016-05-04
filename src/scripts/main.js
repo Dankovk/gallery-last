d26a18874af9ace2a9a692e87936920998838426
vlad <vlad.zaliznyi@popel.com.ua>
2016-05-04 13:53:16 +0300

    vlad checkout css

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -86,6 +86,23 @@
     });
 
 
+    $('[name="woocommerce_checkout_place_order"]').val('Continue to PayPal');
+
+    $('.basket > a').on('click', function() {
+        $('.dropdown-content').fadeIn(300);
+    });
+
+    $(document).on("click", function(event){
+        var $trigger = $('.dropdown');
+        if($trigger !== event.target && !$trigger.has(event.target).length){
+            $('.dropdown-content').fadeOut(300);
+        }            
+    });
+
+    if ($('.dropdown-content li').length > 1) $('.basket > a').addClass('has-items');
+
+    $('#comment').attr('rows', '1');
+    $('#reply-title').remove();
 
     new WOW().init();
 })(window.jQuery);
